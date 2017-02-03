9891327954c2936385615a02ef21d1c8ba3457c8
APilarca <angelpill15@gmail.com>
2017-02-02 20:00:15 -0800

    add more options

--- reminder.py
+++ reminder.py
@@ -10,6 +10,13 @@ while True:
 	new_item = raw_input("> ")
 	if new_item == "DONE":
 		break
+	elif new_item == "SHOW":
+		for item in shopping_list:
+			print(item)
+		continue
+	elif new_item == "HELP":
+		print("DONE ends, SHOW shows list")
+		continue
 	shopping_list.append(new_item)
 
 print("Here's your list:")
