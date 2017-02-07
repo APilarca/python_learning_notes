daa1d636da6b6b1c89e45e7c1b905c4bce1af687
APilarca <angelpill15@gmail.com>
2017-02-06 18:38:29 -0800

    add a value checks to game

--- basics/number_game.py
+++ basics/number_game.py
@@ -1,17 +1,28 @@
 import random
 
-# generate ran num from 1 - 10
-secret_num = random.randint(1, 10)
-# get guess
+def game():
+	# generate ran num from 1 - 10
+	secret_num = random.randint(1, 10)
+	guesses = []
 
-while True:
-	guess = int(input("Guess a number between 1 and 10: "))
-	if guess == secret_num:
-		print("You got it! My number was {}".format(secret_num))
-		break
-	elif guess > secret_num:
-		print("Sorry too hi!")
-	else:
-		print("Try lower!")
-# print hit/miss
+	def add_guess(guessed):
+		guesses.append(guessed)
+	# get guess
+	while len(guesses) < 5:
+		try:
+			guess = int(input("Guess a number between 1 and 10: "))
+		except ValueError:
+			print("{} isn't a number, try again!".format(guess))
+		else:
+			if guess == secret_num:
+				print("You got it! My number was {}".format(secret_num))
+				break
+			elif guess > secret_num:
+				print("Sorry too hi!")
+				add_guess(guess)
+			else:
+				print("Too low!")
+				add_guess(guess)
+		# print hit/miss
 
+game()
