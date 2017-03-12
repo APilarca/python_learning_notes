a6f7b752d0d5bc1d748e764a6cd321f9296a069f
APilarca <angelpill15@gmail.com>
2017-03-11 18:56:06 -0800

    create ability to remove items

--- collections/shopping_list.py
+++ collections/shopping_list.py
@@ -12,6 +12,7 @@ def show_help():
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 Enter 'SHOW' to see your current list.
+Enter 'REMOVE' to remove an item from your list.
 """)
 
 def add_to_list(new_item):
@@ -42,6 +43,15 @@ def show_list():
 		index += 1
 		print("-" * 10)
 
+def remove_from_list():
+	show_list()
+	what_to_remove = raw_input("What would you like to remove?\n> ")
+	try:
+		shopping_list.remove(what_to_remove)
+	except ValueError:
+		pass
+	show_list()
+
 show_help()
 
 while True:
@@ -54,6 +64,8 @@ while True:
 	elif new_item.upper() == 'SHOW':
 		show_list()
 		continue
+	elif new_item.upper() == 'REMOVE':
+		remove_from_list()
 	else:
 		add_to_list(new_item)
 
